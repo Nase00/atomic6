286bee879de0565960eb5b3795e8285eb028ffec
nase00 <nase00@gmail.com>
2014-12-28 14:03:21 -0600

    Fixed and cleaned up comment ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,35 +1,30 @@
 $(document).ready(function() {
-  $('.deleteBlogs').click(function(){
-    $('.toggle').fadeToggle(50)
-  })
 
-  // Fuck this is messy and not DRY
-  
-  toggle = {
-    comments: function comments() {
-      $('.commentsToggle').click(function(){
-        if($(this).val().match(/Display Comments: \d*/)) {
-          var value = $(this).val()
-          value = value.replace("Display Comments:", "Hide Comments:")
-        } else {
-          var value = $(this).val()
-          value = value.replace("Hide Comments:", "Display Comments:")
-        };
-        $(this).val(value)
-        $('.toggle').fadeToggle(50)
-      })
-    }
+  var Toggle = function(clickSelector, toggleSelector) {
+    clickSelector.click(function(e){
+      e.preventDefault()
+      toggleSelector.fadeToggle(50)
+    })
   }
-  
-  toggle.comments();
 
-  $('.makeComment').click(function(e){
-    e.preventDefault()
-    if($('.commentsToggle').val().match(/Hide Comments: \d*/)) {
-      var value = $('.commentsToggle').val()
+  var CommentToggleBar = function(toggle) {
+    if (toggle.val().match(/Display Comments: \d*/)) {
+      var value = toggle.val()
+      value = value.replace("Display Comments:", "Hide Comments:")
+    } else {
+      var value = toggle.val()
       value = value.replace("Hide Comments:", "Display Comments:")
     };
-    $('.toggleMakeComment').fadeToggle(50)
+    toggle.val(value)
+    $('.toggle').fadeToggle(50);
+  }
+
+  Toggle($('.deleteBlogs'), $('.toggle'))
+  Toggle($('.makeComment'), $('.toggleMakeComment'))
+
+  $('.commentsToggle').click(function(e) {
+    e.preventDefault()
+    CommentToggleBar($('.commentsToggle'))
   })
 
   $('#submitComment').click(function(e) {
@@ -42,29 +37,14 @@ $(document).ready(function() {
       dataType: "json",
       data: $("#commentForm").serialize()
     })
-
-    toggle.comments();
     
     $('.toggleMakeComment').fadeToggle(50)
 
     request.done(function(response){
+      CommentToggleBar($('.commentsToggle'))
       $('.newContent').show()
       $('.newPostTitle').append(response.title)
       $('.newPostContent').append(response.content)
     });
   })
-
-
-
-  // $('.deleteBlogs').click(function(e) {
-  //   e.preventDefault()
-  //   var request = $.ajax({
-  //     url: "/blogs",
-  //     method: "get"
-  //   })
-
-  //   request.done(function(response){
-  //     $('.toggle').fadeToggle(50)
-  //   });
-  // })
 });
