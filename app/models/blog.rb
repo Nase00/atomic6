58cc809402086fa718ceb3cdf67c83ce3ab6bf3a
nase00 <nase00@gmail.com>
2014-12-30 04:52:31 -0600

    added more validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ActiveRecord::Base
 	validates :title, :content, :author_id, presence: true
+	validates :title, length: { maximum: 30 }
+	validates :description, length: { maximum: 60 }
+	validates :content, length: { maximum: 10000 }
 
   belongs_to :author, class_name: "User"
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
 	validates :title, :content, :commenter_id, presence: true
+	validates :title, length: { maximum: 150 }
+	validates :content, length: { maximum: 10000 }
 
   belongs_to :commenter, class_name: "User"
   belongs_to :blog, foreign_key: "commenter_id"
