5d1b6f960a40013062892fab97fdcebc198553cf
nase00 <nase00@gmail.com>
2014-12-31 17:11:12 -0600

    Changed AR time to local

--- config/environment.rb
+++ config/environment.rb
@@ -49,3 +49,5 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+ActiveRecord::Base.default_timezone = :local
