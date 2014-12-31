971afe662843fe2ba6cd215014f3f25b830ad285
nase00 <nase00@gmail.com>
2014-12-31 16:18:51 -0600

    Removed comment title validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-	validates :title, :content, :commenter_id, presence: true
+	validates :content, :commenter_id, presence: true
 	validates :title, length: { maximum: 30 }
 	validates :content, length: { maximum: 500 }
 
