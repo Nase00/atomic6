87bbcefa69a9e58d2a9039447c9b317a9c2aefa3
nase00 <nase00@gmail.com>
2015-01-01 21:52:19 -0600

    cleaned up code comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, :email, :hashed_password, presence: true
+  validates :password, length: { minimum: 8 }
   
   has_many :blogs, foreign_key: "author_id"
   has_many :comments, through: :blogs
@@ -7,13 +8,16 @@ class User < ActiveRecord::Base
   has_many :galleries, foreign_key: "creator_id"
   has_many :projects, through: :galleries
 
+  # has_secure_password
+
   def password=(password)
+    # raise 'Password too short (minimum 6 characters)' if password.length > 6
     self.hashed_password = BCrypt::Password.create(password)
   end
 
   def password
     BCrypt::Password.new(self.hashed_password)
-	end
+  end
 
   def authenticate(password)
     self.password == password
