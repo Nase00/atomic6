ae45eee15e45df32916a42a588c99361f983e7e9
Apprentice <nase00@gmail.com>
2015-01-08 07:49:45 -0600

    comment validations tweaked

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
 	validates :content, :commenter_id, presence: true
-	validates :title, length: { maximum: 30 }
-	validates :content, length: { maximum: 500 }
+	validates :title, length: { maximum: 60 }
+	validates :content, length: { maximum: 750 }
 
   belongs_to :commenter, class_name: "User"
   belongs_to :blog, foreign_key: "blog_id"
