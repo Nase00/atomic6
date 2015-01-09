bf826f7d636f1eafb1eac0e8ece4d0c953d56953
Sean <nase00@gmail.com>
2015-01-09 15:15:47 -0600

    testing new AR finder methods

--- app/controllers/searches.rb
+++ app/controllers/searches.rb
@@ -1,13 +1,6 @@
 get '/blogs/search/:search_term' do
-  # search_term = params[:search_term]
-  # blog = Blog.find_by(title: search_term)
-  # content_type :json
-  # if blog
-  #   blog.to_json
-  # else
-  #   search_term.to_json
-  # end
   search_term = params[:search_term]
+  # blogs = Blog.where('title.includes?(#{search_term}')
   blogs = Blog.where(title: search_term)
   content_type :json
   if blogs
