05cd7cf6a4b0d42620d48b64cea840c6747e2336
nase00 <nase00@gmail.com>
2015-01-10 14:49:48 -0600

    Refactoring yay

--- public/js/searches.js
+++ public/js/searches.js
@@ -1,34 +1,15 @@
 $(document).ready(function() {
   var origin = '/blogs/search'
-  function clearInput() { $('#input').val(''); }
-
-  function fillResults(input, response) {
-    var resultsData = {
-      searchedKeyword: input,
-      searchedParam: 'title',
-      resultBlogs: response
-    };
-
-    var route = origin +'?keyword#' + input;
-    var resultsTemplate = _.template(
-      $('#resultsTemplate').html()
-    );
-
-    $('#resultsAnchor').html(resultsTemplate( resultsData ));
-    pushQuery(route)
-  }
-
-  function noInput(input) {
-    var resultsData = { inputKeyword: input };
-    var resultsTemplate = _.template(input);
-    $('#resultsAnchor').empty()
-    $('#resultsAnchor').html(resultsTemplate( resultsData ));
-  }
-
   var keyword = window.location.hash.replace("#","")
   if (keyword) {
     getResults(keyword)
   };
+
+  function clearInput() {
+    $('#input').val('');
+    $('#resultsAnchor').empty()
+    pushQuery(origin)
+  }
 
   function getResults(input) {
     var request = $.ajax({
@@ -47,21 +28,35 @@ $(document).ready(function() {
     })
   }
 
+  function fillResults(input, response) {
+    var resultsData = {
+      searchedKeyword: input,
+      searchedParam: 'title',
+      resultBlogs: response
+    };
+
+    var route = origin +'?keyword#' + input;
+    var resultsTemplate = _.template(
+      $('#resultsTemplate').html()
+    );
+
+    $('#resultsAnchor').html(resultsTemplate( resultsData ));
+    pushQuery(route)
+  }
+
   $('#searchButton').click(function (e) {
     e.preventDefault();
     var input = $('#input').val();
+    clearInput();
     if (input) {
       getResults(input)
     } else {
-      noInput("<p id='error'>You didn't enter anything!</p>")
+      $('#resultsAnchor').html("<p id='error'>You didn't enter anything!</p>")
     };
-    clearInput();
   })
 
   $('#cancelSearch').click(function (e) {
     e.preventDefault();
-    pushQuery(origin)
-    $('#resultsAnchor').empty()
     clearInput();
   })
 });
